c945d081295ede6295884da024fb5c0377007c5d
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2024-11-24 14:10:49 +0700

    Models: update model

--- app/Models/CompanyModel.php
+++ app/Models/CompanyModel.php
@@ -2,10 +2,62 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class CompanyModel extends Model
 {
+    use HasFactory;
+    
     protected $table = 'companies';
     protected $fillable = ['name', 'email', 'logo', 'website'];
+
+    public $timestamps = true;
+
+    public function getAll()
+    {
+        return $this->all();
+    }
+
+    public function createCompany($data)
+    {
+        if (isset($data['logo'])) {
+            $data['logo'] = $data['logo']->store('logos', 'public');
+        }
+
+        return $this->create($data);
+    }
+
+    public function findCompanyById($companyId)
+    {
+        return $this->findOrFail($companyId); // Cari perusahaan berdasarkan ID
+    }
+
+    public function updateCompany($company, $data)
+    {
+        // Jika ada file logo baru
+        if (isset($data['logo'])) {
+            // Hapus logo lama jika ada
+            if ($company->logo) {
+                Storage::delete('public/' . $company->logo);
+            }
+
+            // Simpan logo baru
+            $data['logo'] = $data['logo']->store('logos', 'public');
+        }
+
+        $company->update($data); // Update data perusahaan
+        return $company;
+    }
+
+    public function deleteCompany($company)
+    {
+        // Hapus logo jika ada
+        if ($company->logo) {
+            Storage::delete('public/' . $company->logo);
+        }
+
+        $company->delete(); // Hapus perusahaan
+    }
 }
--- app/Models/EmployeeModel.php
+++ app/Models/EmployeeModel.php
@@ -2,10 +2,41 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class EmployeeModel extends Model
 {
+    use HasFactory;
+
     protected $table = 'employees';
+    
     protected $fillable = ['first_name', 'last_name', 'company_id', 'email', 'phone'];
+
+    public function company()
+    {
+        return $this->belongsTo(CompanyModel::class);
+    }
+
+    public static function createEmployee(array $data)
+    {
+        return self::create([
+            'first_name' => $data['first_name'],
+            'last_name' => $data['last_name'],
+            'company_id' => $data['company_id'],
+            'email' => $data['email'] ?? null,
+            'phone' => $data['phone'] ?? null,
+        ]);
+    }
+
+    public function updateEmployee(array $data)
+    {
+        return $this->update([
+            'first_name' => $data['first_name'],
+            'last_name' => $data['last_name'],
+            'company_id' => $data['company_id'],
+            'email' => $data['email'] ?? null,
+            'phone' => $data['phone'] ?? null,
+        ]);
+    }
 }
